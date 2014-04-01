55db051dfe9f84f78629509f11dbd1525f170933
rickychet <rcbarker2@wisc.edu>
2014-04-01 18:09:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chapterrific::Application.config.secret_key_base = 'b86f11f5c369b6ec3695a7b92b44753e9601081d209c665cb7407c2d897c0d657788a35448b405bb46e5213156c36c186901a9f90bc046aa50093c241fd9e059'
