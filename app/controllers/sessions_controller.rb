fcf67392bbd4139acdbe1ccc2c5c8fac1fac486e
Nick Etzel <njetzel2@gmail.com>
2014-04-08 17:16:06 -0500

    Sign in functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+  
+  private
+    def sign_in(user)
+      remember_token = User.new_remember_token
+      cookies.permanent[:remember_token] = remember_token
+      user.update_attribute(:remember_token, User.hash(remember_token))
+      self.current_user = user
+    end
+end
