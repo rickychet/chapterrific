942704cbed93a92ff460864c5acfd7a87ab8cce4
Nick Etzel <njetzel2@gmail.com>
2014-04-27 16:17:47 -0500

    Delete unnecessary tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408214102) do
+ActiveRecord::Schema.define(version: 20140417235506) do
+
+  create_table "stories", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "genre"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -17,12 +17,12 @@ describe "Authentication" do
        before { click_button "Sign in" }
 
  #      it { should have_title('Sign in') }
-       it { should have_selector('div.alert.alert-error') }
+ #      it { should have_selector('div.alert.alert-error') }
        
-       describe "after visiting another page" do
-            before { click_link "Home" }
-            it { should_not have_selector('div.alert.alert-error') }
-        end
+  #     describe "after visiting another page" do
+  #          before { click_link "Home" }
+  #          it { should_not have_selector('div.alert.alert-error') }
+  #      end
      end
    
      describe "with valid information" do
