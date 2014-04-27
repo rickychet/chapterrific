e53a450838e425e379f32aecdd6703c55d5a9998
Nick Etzel <njetzel2@gmail.com>
2014-04-27 16:25:20 -0500

    Add spaces

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,8 @@ describe User do
   subject { @user }
   
   it { should respond_to(:username) }
-  it {should respond_to(:email)}
-  it {should respond_to(:bio)}
+  it { should respond_to(:email) }
+  it { should respond_to(:bio) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
