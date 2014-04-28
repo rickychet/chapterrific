0300bfb3cdbd04fc3fca71873fab59005422b3ae
Nick Etzel <njetzel2@gmail.com>
2014-04-28 12:45:22 -0500

    Fixed story pages spec

--- spec/requests/story_pages_spec.rb
+++ spec/requests/story_pages_spec.rb
@@ -6,14 +6,16 @@ describe "Story pages" do
   
   let(:user) { FactoryGirl.create(:user) }
   
-  before { sign_in user }
-  let(:story) { FactoryGirl.create(:story, user: user) }
-  
   describe "view story" do
-    before { visit story_path(story) }
     
-    it should have_content(story.title)
-    it should have_content(story.body)
+    let(:story) { FactoryGirl.create(:story, user: user) }
+    before do
+      sign_in user
+      visit story_path(story)
+    end
+    
+    it {  should have_content(story.title) }
+    it { should have_content(story.body) }
   end
 end
     
