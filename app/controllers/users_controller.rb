8084eb8f38e0de9b6033b1219a3e8ec52657d359
rickychet <rcbarker2@wisc.edu>
2014-04-29 11:20:19 -0500

    Add User biograpgies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   
   def create
     @user = User.new(user_params)
@@ -22,7 +36,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:username, :email, :password,
+      params.require(:user).permit(:username, :email, :bio, :password,
                                    :password_confirmation)
     end
     
