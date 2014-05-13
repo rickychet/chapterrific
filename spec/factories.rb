e039be4d4d98cabd44a513126008cb0487934e0a
rickychet <rcbarker2@wisc.edu>
2014-05-13 17:54:31 -0500

    tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,6 +5,13 @@ FactoryGirl.define do
     password "foobar"
     password_confirmation "foobar"
   end
+
+  factory :user2 do
+    username     "uzer2"
+    email    "uzer2@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
   
   factory :story do
     title "The Raven"
@@ -37,4 +44,23 @@ That I scarce was sure I heard you' - here I opened wide the door; -
 Darkness there, and nothing more."
     user
   end
+
+  # factory :storywithlimits do
+  #   title "Moby Dick"
+  #   body "Call me Ishmael. Some years ago—never mind how long precisely
+  #   —having little or no money in my purse, and nothing particular to 
+  #   interest me on shore, I thought I would sail about a little and see 
+  #   the watery part of the world. It is a way I have of driving off the 
+  #   spleen, and regulating the circulation. Whenever I find myself growing 
+  #   grim about the mouth; whenever it is a damp, drizzly November in my 
+  #   soul; whenever I find myself involuntarily pausing before coffin 
+  #   warehouses, and bringing up the rear of every funeral I meet; and 
+  #   especially whenever my hypos get such an upper hand of me, that it 
+  #   requires a strong moral principle to prevent me from deliberately 
+  #   stepping into the street, and methodically knocking people's hats 
+  #   off—then, I account it high time to get to sea as soon as I can."
+  #   lower_limit 20
+  #   upper_limit 50
+  #   user
+  # end
 end
--- spec/requests/story_pages_spec.rb
+++ spec/requests/story_pages_spec.rb
@@ -33,17 +33,17 @@ describe "Story pages" do
     end
     
     describe "with valid information" do
-    before do
-      fill_in "Title",    with: "The Raven"
-      fill_in "Body", with: "Once upon a midnight dreary, while I pondered weak and weary"
-    end
-    it "should create a story" do
-      expect { click_button "Create Story" }.to change(Story, :count).by(1)
+      before do
+        fill_in "Title",    with: "The Raven"
+        fill_in "Body", with: "Once upon a midnight dreary, while I pondered weak and weary"
+      end
+      it "should create a story" do
+        expect { click_button "Create Story" }.to change(Story, :count).by(1)
+      end
     end 
   end
-end
   
-  describe "view story" do
+  describe "view/edit story" do
     
     let(:story) { FactoryGirl.create(:story, user: user) }
     before do
@@ -56,23 +56,64 @@ end
     
     describe "change story content" do
       let(:oldbody) { story.body }
+
       describe "before submitting edits" do
         it { should have_content(story.title) }
         it { should have_content(story.body) }
       end
       
       describe "after submitting edits" do
-      before do
-        visit edit_story_path(story)
-        fill_in "Addition", with: "New content"
-        click_button "Add to Story"
+        before do
+          visit edit_story_path(story)
+          fill_in "Addition", with: "New content"
+          click_button "Add to Story"
+        end
+        it { should have_content(story.title) }
+        it { should have_content(story.body) }
+        it { should have_content(oldbody) }
+        it { should have_content('New content') }
       end
-      it { should have_content(story.title) }
-      it { should have_content(story.body) }
-      it { should have_content(oldbody) }
     end
-      
+
+    describe "change story content with limits" do
+      let(:newuser) { FactoryGirl.create(:user) }
+      let(:limit_story) { FactoryGirl.create(:story, user: user) } #limits: lower-20 upper-50
+      let(:old_body) { limit_story.body }
+      before do
+        sign_in user
+        visit edit_story_path(limit_story)
+      end
+
+      subject { limit_story.body }
+
+      describe "addition too short" do
+        before do
+          fill_in "Addition", with: "Too short"
+          click_button "Add to Story"
+        end
+
+        it { should_not have_content("Too short") }
+      end
+
+      describe "addition too long" do
+        before do
+          visit story_path(limit_story)
+          fill_in "Addition", with: "This string is too long, which means it shouldn't be added to the story"
+          click_button "Add to Story"
+        end
+
+        it { should_not have_content("This string is too long, which means it shouldn't be added to the story") }
+      end
+
+      describe "addition just right" do
+        before do
+          visit story_path(limit_story)
+          fill_in "Addition", with: "This string is the perfect length"
+          click_button "Add to Story"
+        end
+
+        it { should have_content("This string is the perfect length") }
+      end
     end
   end
 end
-    
