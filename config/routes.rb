2c06d411d71c609bff43087c74758c344b8180c1
rickychet <rcbarker2@wisc.edu>
2014-05-13 18:55:52 -0500

    add help page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Chapterrific::Application.routes.draw do
   match '/signup', to: 'users#new', via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/help',    to: 'static_pages#help',    via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
