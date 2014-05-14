e0f4cb1409c15f48657e7babbf54931c8428abb5
rickychet <rcbarker2@wisc.edu>
2014-05-13 20:56:32 -0500

    1 test fail for one editor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513025542) do
+ActiveRecord::Schema.define(version: 20140514003224) do
 
   create_table "stories", force: true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140513025542) do
     t.text     "addition"
     t.integer  "lower_limit"
     t.integer  "upper_limit"
+    t.integer  "editor_id"
   end
 
   create_table "users", force: true do |t|
