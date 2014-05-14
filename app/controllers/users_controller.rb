35c38226083dbe6c86484879045c51305ba63a55
Nick Etzel <njetzel2@gmail.com>
2014-05-13 19:38:52 -0500

    Fixed user registration issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
- #     sign_in @user
-#      flash[:success] = "Welcome to the Sample App!"
+      sign_in @user
+      flash.now[:success] = "Welcome to Chapterrific!"
       redirect_to @user
     else
       render 'new'
